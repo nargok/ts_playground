e931ed382b8176da0c88516f42f96f7f7991955b
Ryoichi Gokan <nargok@gmail.com>
2020-08-26 14:30:39 +0900

    literal

---
+++ src/literal.ts
@@ -0,0 +1,10 @@
+export {}
+
+type DayOfTheWeek = '日' | '月' | '火' | '水' | '木' | '金' | '土';
+
+let dayOfTheweek: DayOfTheWeek = '日';
+dayOfTheweek = '月';
+// dayOfTheweek = '1';
+
+type Month = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+let month : Month = 3;
