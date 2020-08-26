917e9b43cdb44856f1fc5923069e8a912e223a7c
Ryoichi Gokan <nargok@gmail.com>
2020-08-26 10:59:37 +0900

    object type

---
+++ src/object.ts
@@ -0,0 +1,7 @@
+export {}
+
+let profile1: object = { name: 'fuga' };
+profile1 = { birthYear: 1990 };
+
+let profile2: { name: string } = { name: 'hello' };
+// profile2 = { birthYear: 1990 }; // error!
