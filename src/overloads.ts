5e7bff4b624c8429bbe77c09738b8f6c372f4c78
Ryoichi Gokan <nargok@gmail.com>
2020-08-28 11:16:07 +0900

    overload 2nd

--- src/overloads.ts
+++ src/overloads.ts
@@ -4,29 +4,16 @@ export {}
 function double(value: number): number;
 function double(value: string): string;
 
-
 // 実体、シグネチャで型制約を実現しているのでany型を使って構わない
 function double(value: any): any {
   console.log(typeof value);
   if (typeof value === "number") {
     return value * 2;
-  } else if (typeof value === "string") {
-    return value + value;
   } else {
-    throw 'value type is not supported';
+    return value + value;
   }
 }
 
-// function double(value: number): number {
-//   return value * 2;
-// }
-
 console.log(double(100));
-
-// function double(value: string): string {
-//   return value + value;
-// }
-
 console.log(double('Go '));
-
 // console.log(double(true)); // error!
